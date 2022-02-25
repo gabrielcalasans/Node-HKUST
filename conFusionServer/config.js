344ee205520ef366c9ac9e64db2808af485fe9d3
Gabriel <gcalasans71@gmail.com>
2022-02-25 17:48:01 -0300

    Passport Facebook Auth

--- conFusionServer/config.js
+++ conFusionServer/config.js
@@ -1,4 +1,9 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientID: '502627191374019',
+        clientSecret: 'dbc549bf4502ed813e2a46c29ff68f8a'
+        
+    }
 }
